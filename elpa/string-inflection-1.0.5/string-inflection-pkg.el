7703532d2c313a47c5c7e5f0f55609e9bf8b50c0
ekr <ekr@ekrdev.nodomain>
2017-02-09 16:45:02 +0100

    string-inflection hinzu

---
+++ elpa/string-inflection-1.0.5/string-inflection-pkg.el
@@ -0,0 +1,1 @@
+(define-package "string-inflection" "1.0.5" "underscore -> UPCASE -> CamelCase -> lowerCamelCase conversion of names" 'nil :keywords '("elisp"))
--- init.el
+++ init.el
@@ -631,3 +631,19 @@
 
 (run-at-time "1" nil '(lambda () (toggle-frame-maximized)))
 
+(require 'string-inflection)
+(global-unset-key (kbd "C-q"))
+(global-set-key (kbd "C-q C-u") 'my-string-inflection-cycle-auto)
+(defun my-string-inflection-cycle-auto ()
+  "switching by major-mode"
+  (interactive)
+  (cond
+   ;; for emacs-lisp-mode
+   ((eq major-mode 'emacs-lisp-mode)
+    (string-inflection-all-cycle))
+   ;; for java
+   ((eq major-mode 'java-mode)
+    (string-inflection-java-style-cycle))
+   (t
+    ;; default
+    (string-inflection-ruby-style-cycle))))
