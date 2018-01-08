7e97bda7899aa6b3e40fa7e2474e77d0d11e7399
Ekkehard Krämer <ekkehard.kraemer@nttdata.com>
2018-01-08 13:57:00 +0100

    find -print0 | xargs grep -0

--- init.el
+++ init.el
@@ -102,7 +102,7 @@
  '(grep-find-ignored-files
    (quote
     (".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo" "*.gz")))
- '(grep-find-template "find . <X> -type f <F> -print | xargs grep <C> -n <R>")
+ '(grep-find-template "find . <X> -type f <F> -print0 | xargs -0 grep <C> -n <R>")
  '(icicle-guess-commands-in-path nil)
  '(icicle-redefine-standard-commands-flag nil)
  '(ido-confirm-unique-completion t)
